6ba795b7690649bc5bc0ba23f5d19d4405821426
Macgyver Martins <macgyver.martins@gmail.com>
2017-11-02 14:14:27 -0300

    Add initial models

---
+++ test/system/kinds_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class KindsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit kinds_url
+  #
+  #   assert_selector "h1", text: "Kind"
+  # end
+end
